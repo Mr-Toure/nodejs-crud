ad958aee4d133184d779f56d9c176dc4aa3c2564
Toure Simplice <toure-simplice@outlook.fr>
2021-12-16 14:36:29 +0000

    basic crud finish

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -25,7 +25,8 @@ exports.view = (req, res) =>
             connection.release();
 
             if(!err){
-                res.render('home', { rows });
+                let removedUser = req.query.removed;
+                res.render('home', { rows, removedUser });
             }else{
                 console.log(err);
             }
@@ -77,7 +78,7 @@ exports.create = (req, res) =>{
         let searchTerm = req.body.search;
 
         //User the connection
-        connection.query('INSERT INTO user SET first_name = ?, last_name = ?, email = ?, phone = ?, comments = ?', [first_name, last_name, email, phone, comments], (err, rows) => {
+        connection.query('INSERT INTO user SET first_name = ?, last_name = ?, email = ?, comment = ?, phone = ?', [first_name, last_name, email, comments, phone], (err, rows) => {
             //when donne with connection release it
             connection.release();
             if(!err){
@@ -89,3 +90,124 @@ exports.create = (req, res) =>{
         });
     });
 }
+
+//Edit interface add new user
+exports.edit = (req, res) => {
+    //connect to database
+    pool.getConnection((err, connection) => 
+    {
+        if (err) throw err; //not connect
+        console.log('connected as ID '+ connection.threadId);
+
+        //User the connection
+        connection.query('SELECT * FROM user WHERE id = ?', [req.params.id], (err,rows) => 
+        {
+            //when donne with connection release it
+            connection.release();
+
+            if(!err){
+                res.render('edit-user', { rows });
+            }else{
+                console.log(err);
+            }
+            console.log('The data From user table: \n', rows);
+        });
+    });
+}
+
+//update interface add new user
+exports.update = (req, res) => {
+
+    const {first_name, last_name, email, phone, comments } = req.body;
+    //connect to database
+    pool.getConnection((err, connection) => 
+    {
+        if (err) throw err; //not connect
+        console.log('connected as ID '+ connection.threadId);
+
+        //User the connection
+        connection.query('UPDATE user SET first_name = ?, last_name = ?, email = ?, comment = ?, phone = ? WHERE id = ?', [first_name, last_name, email, comments, phone, req.params.id], (err,rows) => 
+        {
+            //when donne with connection release it
+            connection.release();
+
+            if(!err){
+                //connect to database
+                pool.getConnection((err, connection) => 
+                {
+                    if (err) throw err; //not connect
+                    console.log('connected as ID '+ connection.threadId);
+
+                    //User the connection
+                    connection.query('SELECT * FROM user WHERE id = ?', [req.params.id], (err,rows) => 
+                    {
+                        //when donne with connection release it
+                        connection.release();
+
+                        if(!err){
+                            res.render('edit-user', { rows, alert: "Data updated Successfuly." });
+                        }else{
+                            console.log(err);
+                        }
+                        console.log('The data From user table: \n', rows);
+                    });
+                });
+
+            }else{
+                console.log(err);
+            }
+            console.log('The data From user table: \n', rows);
+        });
+    });
+}
+
+//delete user
+exports.delete = (req, res) => {
+    //connect to database
+    pool.getConnection((err, connection) => 
+    {
+        if (err) throw err; //not connect
+        console.log('connected as ID '+ connection.threadId);
+
+        //User the connection
+        connection.query('DELETE FROM user WHERE id = ?', [req.params.id], (err,rows) => 
+        {
+            //when donne with connection release it
+            connection.release();
+
+            if(!err){
+                let rm = encodeURIComponent('User successefuly removed')
+                 res.redirect('/?removed=' + rm);
+            }else{
+                console.log(err);
+            }
+            console.log('The data From user table: \n', rows);
+        });
+    });
+}
+
+
+//view user
+exports.viewall = (req, res) => 
+{
+    //connect to database
+    pool.getConnection((err, connection) => 
+    {
+        if (err) throw err; //not connect
+        console.log('connected as ID '+ connection.threadId);
+
+        //User the connection
+        connection.query('SELECT * FROM user WHERE id = ?', [req.params.id], (err,rows) => 
+        {
+            //when donne with connection release it
+            connection.release();
+
+            if(!err){
+                res.render('view-user', { rows });
+            }else{
+                console.log(err);
+            }
+            console.log('The data From user table: \n', rows);
+        });
+    });
+};
